13dbcbfc13e02311f1097f2c6b6fa76503465b1c
fyan0125 <elainefang2001@gmail.com>
2021-04-11 23:04:03 +0800

    menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,36 @@
 import { NavLink } from 'react-router-dom';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined, AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import ActivityItem from "./ActivityItem";
 
 export default function NavBar() {
+    const menu = (
+        <Menu>
+          <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+              1st menu item
+            </a>
+          </Menu.Item>
+          <Menu.Item icon={<DownOutlined />} >
+            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+              2nd menu item
+            </a>
+          </Menu.Item>
+          <Menu.Item >
+            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+              3rd menu item
+            </a>
+          </Menu.Item>
+        </Menu>
+    );
+    const { SubMenu } = Menu;
     return (
         <div className="nav-bar">
-            <NavLink to="/" className="nav-item" activeClassName="nav-item--active">
-                煞氣ㄉ音樂祭
-            </NavLink>
+            <Dropdown overlay={menu}>
+                    <NavLink to="/"className="ant-dropdown-link nav-item" activeClassName="nav-item--active" onClick={e => e.preventDefault()}>
+                    煞氣ㄉ音樂祭
+                    </NavLink>
+            </Dropdown>
             <NavLink to="/" className="nav-item" activeClassName="nav-item--active">
                 想去ㄉ活動
             </NavLink>
@@ -15,3 +40,4 @@ export default function NavBar() {
         </div>
     );
 }
+
